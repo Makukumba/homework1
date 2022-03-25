809dfafa26b5abfe70a8e4e6dae2591aa0c4032d
Makukumba <evgrafborezov@yandex.ru>
2022-03-26 01:32:59 +0300

    new commit

--- Ex2_3Ez.java
+++ Ex2_3Ez.java
@@ -6,15 +6,17 @@ public class Ex2_3Ez{
     public static void main(String[] args) throws ExceptionNotInt {
 
         Scanner s = new Scanner(System.in);
+
         String t = s.nextLine();
-        if (s.hasNextInt()) {
+        while (s.hasNextInt()) {
             int a = s.nextInt();
             if (a> 0) {
-                for (int i = 0; i < a; i++) {
+              for (int i = 0; i < a; i++){
                     System.out.println(t);}
-            } } else {
+            } else if (a != (int)a){throw new ExceptionNotInt("Ошибка");}
+            else {
             throw new ExceptionNotInt("Ошибка");
-        }
+        }}
     }
     public static class ExceptionNotInt extends Exception{
         public ExceptionNotInt(String message) {
